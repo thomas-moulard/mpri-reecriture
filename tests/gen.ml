3fa4bc95f48774859a892bba31db1336cdc6b254
Thomas Moulard <thomas.moulard@gmail.com>
2009-01-18 12:02:37 +0100

    Test gen functions.

--- tests/gen.ml
+++ tests/gen.ml
@@ -11,7 +11,24 @@ open Test;;
  ****************************************************************************)
 
 let check_swap_nth_child () =
-  () (* FIXME: *)
+  let eq n value l1 l2 =
+    try
+      List.for_all2 (==) (swap_nth_child l1 n value) l2
+    with Invalid_argument _ -> false
+  and print = print_list print_int_ws in
+  let chk n value = metachk false print print (eq n value) in
+
+  printf "With n = -1:@\n";
+  chk (-1) 0 [] [];
+  chk (-1) 0 [0] [0];
+  chk (-1) 0 [0;1] [0;1];
+
+  printf "With n = 0, val = 99:@\n";
+  chk 0 99 [0] [99];
+  chk 0 99 [0;1] [99;1];
+
+  printf "With n = 2, val = 99:@\n";
+  chk 2 99 [0;1;2] [0;1;99];
 ;;
 
 
