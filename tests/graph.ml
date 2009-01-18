094341a49aad2ed5352c4e89bf02810d9b86b1ec
Thomas Moulard <thomas.moulard@gmail.com>
2009-01-19 00:17:33 +0100

    Test graphs.

--- tests/graph.ml
+++ tests/graph.ml
@@ -10,119 +10,6 @@ open Test;;
  * Check graph functions.                                                   *
  ****************************************************************************)
 
-(*let print_int_space e  = print_int e; print_string " ";;
-
-let print_write_graph sys file =
-  let g = (compute_graph (compute_symbols sys) (compute_dps sys))
-  in
-  print_graph Format.std_formatter g;
-  print_newline ();
-  write_graph_dot file g
-;;
-
-let test_graph_init () =
-  print_string "** Graph init (system 7.3)\n";
-  print_write_graph system_7_3 "graph_7-3.dot";
-
-  print_string "** Graph init (system 7.11)\n";
-  print_write_graph system_7_11 "graph_7-11.dot";
-
-  print_string "** Graph init (system 7.19)\n";
-  print_write_graph system_7_19 "graph_7-19.dot";
-  print_newline ();
-;;
-
-let test_acc g =
-  for i = 0 to graph_nb_nodes g - 1 do
-    print_string "Node ";
-    print_int i;
-    print_string ":\n";
-    print_string "\t Acc: ";
-    print_list print_int_space (graph_acc g i);
-    print_newline ();
-    print_string "\t Coacc: ";
-    print_list print_int_space (graph_coacc g i);
-    print_newline ();
-  done
-;;
-
-
-print_string "* Test acc/coacc on handmade graphs.\n";
-print_string "** System 7.11\n";
-test_acc graph_7_11;
-print_string "** System 7.19\n";
-test_acc graph_7_19;
-print_newline ();
-
-print_string "* Test graph init.\n";
-test_graph_init ();
-
-
-write_graph_dot "handmade_graph_7-11.dot" graph_7_11;
-write_graph_dot "handmade_graph_7-19.dot" graph_7_19;
-*)
-
-(*
-let print_write_graph g file =
-  print_graph Format.std_formatter g;
-  write_graph_dot file g
-;;
-
-let print_write_extr_comps g files =
-  let print_int_space e = print_int e; print_string " " in
-  let gs = extract_components g in
-  begin
-    for i = 0 to pred (graph_nb_nodes g) do
-      print_string "\t Acc: "; print_list print_int_space (graph_acc g i);
-      print_newline ();
-      print_string "\t Co-acc: "; print_list print_int_space (graph_coacc g i);
-      print_newline ();
-    done;
-    print_newline ();
-    let rec print n gs =
-      match gs with
-      | [] -> ()
-      | e::l ->
-          begin
-            print_string "\t *** Component n°"; print_int n; print_newline ();
-            print_write_graph e (Format.sprintf files n);
-            print (n+1) l;
-          end;
-    in
-    print 0 gs;
-    print_string "\n";
-  end
-;;
-
-let print_write_extr_comps_from_sys sys files =
-  let g = compute_graph (compute_symbols sys) (compute_dps sys) in
-  print_write_extr_comps g files
-;;
-
-
-let test_graph_extr_comps () =
-  print_string "* Searchs strong comps on handmade graphs.\n";
-  print_string "** System 7.11\n";
-  print_write_extr_comps graph_7_11 "handmade-graph_7-11-comp-%d.dot";
-  print_string "** System 7.19\n";
-  print_write_extr_comps graph_7_19 "handmade-graph_7-19-comp-%d.dot";
-  print_newline ();
-
-  print_string "* Search using g_init.\n";
-  print_string "** Graph components (system 7.3)\n";
-  print_write_extr_comps_from_sys system_7_3 "graph_7-3-comp-%d.dot";
-
-  print_string "** Graph components (system 7.11)\n";
-  print_write_extr_comps_from_sys system_7_11 "graph_7-11-comp-%d.dot";
-
-  print_string "** Graph components (system 7.19)\n";
-  print_write_extr_comps_from_sys system_7_19 "graph_7-19-comp-%d.dot";
-;;
-
-test_graph_extr_comps ()
-
-*)
-
 
 let eq_graph g1 g2 =
   if graph_nb_nodes g1 != graph_nb_nodes g2 then
@@ -156,6 +43,13 @@ let check_graph_nb_nodes () =
   let g = make_empty_graph 5 in
   chk g 5;
 ;;
+
+
+let rec print_graphs = function
+  | [] -> ()
+  | e::l -> print_graph e; print_graphs l
+;;
+
 
 let check_make_empty_graph () =
   let chk = metachk false print_graph print_graph
@@ -275,13 +169,56 @@ let check_extract_components () =
     printf "@]" in
   let chk = metachk false print_graph print eq in
 
-  chk (make_empty_graph 2) [];
-  chk graph_7_11 [];
-  chk graph_7_19 [];
+  chk (make_empty_graph 2) [make_empty_graph 2; make_empty_graph 2];
+
+  let g1 = make_empty_graph 3
+  and g2 = make_empty_graph 3 in
+  add_edge 1 1 g1;
+  add_edge 0 0 g2;
+
+  chk graph_7_11 [make_empty_graph 3; g1; g2];
+
+  let g1 = make_empty_graph 4
+  and g2 = make_empty_graph 4 in
+  add_edge 1 3 g1;
+  add_edge 3 1 g1;
+  add_edge 3 3 g1;
+  add_edge 0 0 g2;
+
+  chk graph_7_19 [make_empty_graph 4; g1; g2];
 ;;
 
 let check_compute_graph () =
-  () (* FIXME: *)
+  let eq rules = eq_graph (compute_graph (compute_symbols rules) rules) in
+  let chk = metachk false print_system print_graph eq in
+
+  let g = make_empty_graph 2 in
+  add_edge 0 0 g;
+  chk system_7_3 g;
+  write_graph_dot "graph_init_7_3.dot"
+    (compute_graph (compute_symbols system_7_3) system_7_3);
+
+  let g = make_empty_graph 4 in
+  add_edge 0 0 g;
+  add_edge 2 0 g;
+  add_edge 2 1 g;
+  add_edge 2 2 g;
+  add_edge 2 3 g;
+  chk system_7_11 g;
+  write_graph_dot "graph_init_7_11.dot"
+    (compute_graph (compute_symbols system_7_11) system_7_11);
+
+  let g = make_empty_graph 6 in
+  add_edge 5 0 g;
+  add_edge 5 1 g;
+  add_edge 5 2 g;
+  add_edge 5 3 g;
+  add_edge 5 4 g;
+  add_edge 5 5 g;
+
+  chk system_7_19 g;
+  write_graph_dot "graph_init_7_19.dot"
+    (compute_graph (compute_symbols system_7_19) system_7_19);
 ;;
 
 
